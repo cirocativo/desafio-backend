24f131bbbfe5a5bf48396b1e8d804956d845020c
Ciro Cativo <cirocativo@gmail.com>
2023-01-24 23:06:24 -0400

    Revert "cnab is successfully converted to object"
    This reverts commit 685ab59d9efde76af254f8900ab03716cd2483b5.

--- transactions/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('upload/', views.upload_file, name='upload_file'),
-    path('success/', views.success, name = 'success'),
-]
